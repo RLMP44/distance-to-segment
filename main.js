151f2ed2802e4989d8f2448455cc283e9e0bd98d
Rachael Momber <rlmomber@gmail.com>
2024-04-17 17:03:09 +0900

    fix: refactor

--- main.js
+++ main.js
@@ -21,40 +21,52 @@ function redrawAll() {
   // drawArrow(B);
   // drawArrow(M);
 
-  projectPointToSegment(M, A, B);
+  const { point, t } = projectPointToSegment(M, A, B);
+  drawDot(point, 'point');
 }
 
 function projectPointToSegment(M, A, B) {
   const AB = subtract(B, A);
-  drawArrow(add(AB, A), A);
-
   const AM = subtract(M, A);
-  drawArrow(add(AM, A), A);
-
-  // scaling by the inverse of it's length
-  // const nAB = normalize(AB);
-  // const nAM = normalize(AM);
-  // drawArrow(add(nAM, A), A);
-
-  // displays the direction relative to the current segment
-  // const t = dot(nAM, nAB);
-  // drawText(t);
-
-  // scaling by the inverse of it's length
   const nAB = normalize(AB);
-
-  // displays the distance from point A
-  // to the point's (would be) intersection with the segment
   const t = dot(AM, nAB) / distance(A, B);
-  // to project a point
   const scaler = dot(AM, nAB);
-  drawText(t);
-
-  // gets a point between A and B that is t% away from A using linear interpolation
-  // const point = lerp2D(A, B, t);
-  // drawDot(point, 'point');
-
-  // gets a point between A and B that is t% away from A
   const point = add(A, scale(nAB, scaler));
-  drawDot(point, 'point');
+  return { point, t };
 }
+
+// ----------- before refactor -------------- //
+// function projectPointToSegment(M, A, B) {
+//   const AB = subtract(B, A);
+//   drawArrow(add(AB, A), A);
+
+//   const AM = subtract(M, A);
+//   drawArrow(add(AM, A), A);
+
+//   // scaling by the inverse of it's length
+//   // const nAB = normalize(AB);
+//   // const nAM = normalize(AM);
+//   // drawArrow(add(nAM, A), A);
+
+//   // displays the direction relative to the current segment
+//   // const t = dot(nAM, nAB);
+//   // drawText(t);
+
+//   // scaling by the inverse of it's length
+//   const nAB = normalize(AB);
+
+//   // displays the distance from point A
+//   // to the its projected point along the segment
+//   const t = dot(AM, nAB) / distance(A, B);
+//   // to project a point
+//   const scaler = dot(AM, nAB);
+//   drawText(t);
+
+//   // gets a point between A and B that is t% away from A using linear interpolation
+//   // const point = lerp2D(A, B, t);
+//   // drawDot(point, 'point');
+
+//   // gets a point between A and B that is t% away from A
+//   const point = add(A, scale(nAB, scaler));
+//   drawDot(point, 'point');
+// }
